5a4d3bed71d69013bd0d6103776bc269f3f30664
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-05-11 19:50:48 -0300

    coloquei hora completa e data

--- script.js
+++ script.js
@@ -31,3 +31,51 @@ const blocoCor2 = criarElemento("div", "cor2")
 blocoCores.appendChild(blocoCor2)
 const blocoCor3 = criarElemento("div", "cor3")
 blocoCores.appendChild(blocoCor3)
+
+
+const data = new Date()
+const nomeMeses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+const dia = data.getDate()
+const mes = nomeMeses [data.getMonth()]
+const ano = data.getFullYear()
+
+
+const dataComposta = `${dia} de ${mes} de ${ano}`
+const blocoTitulo = document.createElement("h3")
+blocoTitulo.classList.add("bloco__titulo")
+blocoTitulo.innerText = dataComposta
+bloco.appendChild(blocoTitulo)
+
+
+const blocoSubtitulo = document.createElement("h4")
+blocoSubtitulo.classList.add("bloco__subtitulo")
+blocoSubtitulo.innerText = `${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`
+
+bloco.appendChild(blocoSubtitulo)
+
+setInterval( function() {
+    blocoSubtitulo.innerText = ""
+    const dataHora = new Date()
+    let hora = dataHora.getHours()
+    if( hora < 10){
+        hora = `0${hora}`
+    }
+    let minutos = dataHora.getMinutes()
+    if( minutos < 10){
+        minutos = `0${minutos}`
+    }
+    let  segundos = dataHora.getSeconds() 
+    if( segundos < 10){
+        segundos = `0${segundos}`
+    }
+
+    const horaComposta = `${hora}:${minutos}:${segundos}`
+    blocoSubtitulo.innerText = horaComposta
+}, 1000)
+
+
+
+
+
+
+
